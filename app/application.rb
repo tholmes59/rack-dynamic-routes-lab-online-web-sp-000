2aa6b924601fa16ae9f64f126f7fc9ec124d5bd2
Tom Holmes <tholmes59@gmail.com>
2019-03-07 23:46:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,25 +1,5 @@
 class Application
  
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
- 
-    if req.path=="/items"
-      resp.write "You requested the songs"
-    else
-      resp.write "Route not found"
-      resp.status = 404
-    end
- 
-    resp.finish
-  end
-end
-
-
-
-
-class Application
- 
   @@items = []
  
   def call(env)
