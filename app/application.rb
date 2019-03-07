747fcaa63278930a614600eebc494e5471f3dc35
Tom Holmes <tholmes59@gmail.com>
2019-03-07 23:49:42 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,7 @@ class Application
       item_name = req.path.split("/items/").last #turn 
       item = @@items.find{|s| s.name == item_name}
  
-      resp.write song.price
+      resp.write item.price
     end
     else
       resp.write "Route not found"
@@ -30,3 +30,27 @@ class Application
     resp.finish
   end
 end
+
+# class Application
+
+#   @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
+#   def call(env)
+#     resp = Rack::Response.new
+#     req = Rack::Request.new(env)
+
+#     if req.path.match(/items/)
+#       item_name = req.path.split("/items/").last
+#       if item =@@items.find{|i| i.name == item_name}
+#         resp.write item.price
+#       else 
+#         resp.status = 400
+#         resp.write "Item not found"
+#       end
+#     else
+#       resp.status=404
+#       resp.write "Route not found"
+#     end
+#     resp.finish
+#   end
+
+# end
